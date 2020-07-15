2e8f57cca0bae8866d2b2f62748bfd2ae4cb2cb4
Ciro-Lopes <ciro.flopes@gmail.com>
2020-07-15 17:32:45 -0300

    Criação do Projeto e Configuração do Multer

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan());
+
+app.use(routes);
+
+app.listen(3000);
